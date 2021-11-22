fixed flipper rotation and code cleanup

src/pinball_wgui.py
@@ -13,6 +13,7 @@ import time
 
     TODO: add GUI stuff
 '''
+
 cWidth=400
 cHeight=600
 flipperHeight = 1.7
@@ -20,20 +21,16 @@ cScale = cHeight / flipperHeight
 simWidth = cWidth / cScale
 simHeight = cHeight / cScale
 window = tk.Tk()
-c = tk.Canvas(window,bg="black",width=cWidth,height=cHeight)
-c.pack()
-
-left_flip = False
-right_flip = False
+canvas = tk.Canvas(window,bg="black",width=cWidth,height=cHeight)
+canvas.pack()
 
 
-
-# utility function
+# utility functions
 def vector_length(x):
     '''returns the length of a vector'''
     return np.sqrt(x.dot(x))    # faster than np.linalg.norm(x)
 
-#This Function contains Errors
+
 def closest_point_on_segment(p, a, b):
     ab = b - a
     t = ab.dot(ab)
@@ -44,6 +41,14 @@ def closest_point_on_segment(p, a, b):
 
     t = max(0.0, min(1.0, (p.dot(ab) - a.dot(ab)) / t))
     return a + ab * t
+
+
+def cX(xpos):
+	return xpos * cScale
+
+
+def cY(ypos):
+	return cHeight - ypos * cScale
 
 
 class Ball:
@@ -62,7 +67,7 @@ class Ball:
     
 
 class Flipper:
-    def __init__(self, pos, length, radius, rest_angle, max_rotation, angular_vel):
+    def __init__(self, pos, length, radius, rest_angle, max_rotation, angular_vel, window, button):
         self.pos = pos
         self.length = length
         self.radius = radius
@@ -70,20 +75,20 @@ class Flipper:
         self.max_rotation = abs(max_rotation)
         self.sign = math.copysign(1, max_rotation)
         self.angular_vel = angular_vel
-        #dynamic
+        
         self.rotation = 0.0
         self.current_angular_vel = 0.0
-        self.touch_id = -1
+        self.is_pressed = False
+
+        window.bind(f'<KeyPress-{button}>', self.activate)
+        window.bind(f'<KeyRelease-{button}>', self.deactivate)
 
     
-    def simulate(self, dt, pressed):
-        #global left_flip
+    def simulate(self, dt):
         prev_rotation = self.rotation
-        #pressed = left_flip
         
-        if (pressed):
-            self.rotation = 40 + min(self.rotation + dt * self.angular_vel, self.max_rotation-40)
-            #left_flip = False
+        if (self.is_pressed):
+            self.rotation = min(self.rotation + dt * self.angular_vel, self.max_rotation)
         else:
             self.rotation = max(self.rotation - dt * self.angular_vel, 0.0)
         
@@ -93,6 +98,39 @@ class Flipper:
     def select(self, pos):
         dist = self.pos - pos
         return vector_length(dist) - vector_length(self.length)
+
+
+    def activate(self, event):
+        self.is_pressed = True
+
+    
+    def deactivate(self, event):
+        self.is_pressed = False
+
+
+    def rotate(self, angle):
+        #Rotation: Translate Middle to the Origin, do simple rotation, translate back
+        #Clockwise 45 degrees = r = np.array(((np.cos(theta),-np.sin(theta)),(np.sin(theta),np.cos(theta))))
+        theta = np.radians(angle)
+        trans_x = self.pos[5][0]
+        trans_y = self.pos[5][1]
+
+        c = np.copy(self.pos)
+        # rotation
+        for i in c:
+            #NP Array of 2D Vect
+            #Translate to Origin
+            i[0] = i[0] - trans_x
+            i[1] = i[1] - trans_y
+            tmpx = i[0]
+            tmpy = i[1]
+
+            i[0] = np.cos(theta) * tmpx + (-np.sin(theta)) * tmpy
+            i[1] = np.sin(theta) * tmpx + np.cos(theta) * tmpy
+                
+            i[0] = i[0] + trans_x
+            i[1] = i[1] + trans_y
+        return c
 
 
 
@@ -116,44 +154,9 @@ class PhysicsScene:
         self.score = 0
         self.paused = True
 
-def cX(xpos):
-	return xpos * cScale
 
-def cY(ypos):
-	return cHeight - ypos * cScale
-
-
-def setup_canvas():
-    # TODO: gui setup
-    
-     #Function that runs the window + event listener
-    pass
-
-def flipper_angle(f,angle):
-    #Rotation: Translate Middle to the Origin, do simple rotation, translate back
-    #Clockwise 45 degrees = r = np.array(((np.cos(theta),-np.sin(theta)),(np.sin(theta),np.cos(theta))))
-    theta = np.radians(angle)
-    r = np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
-    trans_x = f.pos[5][0]
-    trans_y = f.pos[5][1]
-
-    c = np.copy(f.pos)
-    # rotation
-    for i in c:
-        #NP Array of 2D Vect
-        #Translate to Origin
-        i[0] = i[0] - trans_x
-        i[1] = i[1] - trans_y
-        tmpx = i[0]
-        tmpy = i[1]
-
-        i[0] = np.cos(theta) * tmpx + (-np.sin(theta)) * tmpy
-        i[1] = np.sin(theta) * tmpx + np.cos(theta) * tmpy
-            
-        i[0] = i[0] + trans_x
-        i[1] = i[1] + trans_y
-    return c
 def setup_scene() -> PhysicsScene:
+    global window
     #scene borders --> Define set of pixel pairs
     #border = [0.0,0.0, 0.0,800, 300,1100, 300,1200, 500,1200, 500,1100, 800,800, 800,0.0]
     #border = [x/2 for x in border]
@@ -184,14 +187,14 @@ def setup_scene() -> PhysicsScene:
     length = 50
     max_rotation = -80
     rest_angle = 40
-    angular_vel = 200
+    angular_vel = 700
     restitution = 1.0
     x1 = cWidth * 0.2 # 300
     y1 = cHeight * 0.9 # 1100
     x2 = cWidth * 0.8 # 500
     y2 = cHeight * 0.9 # 1100
-    flipper1 = Flipper(np.array([[x1,y1+radius+radius], [x1+length,y1+radius+radius], [x1+length,y1+radius], [x1+length,y1], [x1,y1], [x1,y1+radius]]), length, radius, rest_angle, max_rotation, angular_vel)
-    flipper2 = Flipper(np.array([[x2,y2+radius+radius], [x2-length,y2+radius+radius], [x2-length,y2+radius], [x2-length,y2], [x2,y2], [x2,y2+radius]]), length, radius, -rest_angle, -max_rotation, angular_vel)
+    flipper1 = Flipper(np.array([[x1,y1+radius+radius], [x1+length,y1+radius+radius], [x1+length,y1+radius], [x1+length,y1], [x1,y1], [x1,y1+radius]]), length, radius, rest_angle, max_rotation, angular_vel, window, 'a')
+    flipper2 = Flipper(np.array([[x2,y2+radius+radius], [x2-length,y2+radius+radius], [x2-length,y2+radius], [x2-length,y2], [x2,y2], [x2,y2+radius]]), length, radius, -rest_angle, -max_rotation, angular_vel, window, 'd')
     flippers = [flipper1, flipper2]
 
     physics_scene = PhysicsScene(border, balls, obstacles, flippers)
@@ -199,13 +202,18 @@ def setup_scene() -> PhysicsScene:
 
 
 def draw_disc(x, y, radius, col):
-    return c.create_oval(x-radius, y-radius, x+radius, y+radius, fill=col,outline='')
+    return canvas.create_oval(x-radius, y-radius, x+radius, y+radius, fill=col,outline='')
+
 
 def draw(physics_scene):
-    # TODO: add
+    global canvas
+    # if we don't delete all canvas objects, tkinter will keep all objects in memory and always create new ones when drawing another object
+    # this will create performance issues and potentially out of memory issue
+    # TODO: maybe use "move" method for certain objects instead of redrawing (?)
+    canvas.delete("all")
+    
     #Draw Frame around GUI:
-    #c.create_rectangle(0,0,cWidth,cHeight,outline="green")
-    c.create_polygon(physics_scene.border.flatten().tolist(),outline="black",fill="white")
+    canvas.create_polygon(physics_scene.border.flatten().tolist(),outline="black",fill="white")
 
 
     #Draw the balls:
@@ -219,14 +227,13 @@ def draw(physics_scene):
   
     #Draw the flippers
     for f in physics_scene.flippers:
-        print(f.rest_angle + f.rotation*f.sign)
-        new_coords = flipper_angle(f,f.rest_angle + f.rotation * f.sign)
+        new_coords = f.rotate(f.rest_angle + f.rotation * f.sign)
         coords = new_coords.flatten().tolist()
-        c.create_polygon(coords, fill = "red")
+        canvas.create_polygon(coords, fill = "red")
         draw_disc(coords[4], coords[5], f.radius,"red")
         draw_disc(coords[10], coords[11],f.radius,"red")
 
-    c.update()
+    canvas.update()
 
 
 def handle_ball_ball_collision(ball1: Ball, ball2: Ball):
@@ -344,21 +351,12 @@ def handle_ball_border_collision(ball: Ball, border):
     new_v = abs(v) * ball.restitution
     ball.vel += d * (new_v - v)
 
-def set_left_flip(event):
-    global left_flip
-    left_flip = True
-
-def set_right_flip(event):
-    global right_flip
-    right_flip = True
 
 def simulate(physics_scene: PhysicsScene):
     draw(physics_scene)
-    global left_flip
-    global right_flip
     
-    physics_scene.flippers[0].simulate(physics_scene.dt, left_flip)
-    physics_scene.flippers[1].simulate(physics_scene.dt, right_flip)
+    physics_scene.flippers[0].simulate(physics_scene.dt)
+    physics_scene.flippers[1].simulate(physics_scene.dt)
 
     for i in range(len(physics_scene.balls)):
         ball = physics_scene.balls[i]
@@ -382,27 +380,21 @@ def simulate(physics_scene: PhysicsScene):
 
     
 def update(physics_scene: PhysicsScene):
-    global left_flip
-    global right_flip
     draw(physics_scene)
     while True:
         simulate(physics_scene)
-        left_flip = False
-        right_flip = False
         time.sleep(physics_scene.dt)
-        
+
+
 def main():
-    setup_canvas()
-    physics_scene = setup_scene()
-    #update(physics_scene)
+    physics_scene = setup_scene()    
     draw(physics_scene)
 
-window.bind('<KeyPress-a>', set_left_flip)
-window.bind('<KeyPress-d>', set_right_flip)
 
 start_button = tk.Button(window, text='START', font=('arial bold', 18), height=10, width=10,
-    bg="black", fg="white", activebackground="green", relief="raised", command=lambda:update(setup_scene()))
+    bg="black", fg="white", activebackground="green", relief="raised", command=lambda:(update(setup_scene())))
 start_button.pack(side=tk.BOTTOM, anchor=tk.S)
+
 
 if __name__ == "__main__":
     main()